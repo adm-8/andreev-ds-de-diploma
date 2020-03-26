927896d1019b0f747ebe6bd782f19b8491172bc1
Diman <dslads@gmail.com>
2020-03-26 10:09:04 +0300

    changes in data generator

--- python/data_generator.py
+++ python/data_generator.py
@@ -7,19 +7,34 @@ loan_ammount = range(192540, 2000000, 13852)
 preiod = range(12, 120, 12)
 
 def get_opty(with_result = True):
-    if with_result == True:
-        sal = choice(salary)
-        la = choice(loan_ammount)
-        per = choice(preiod)
-        req_percent = (la / per) / sal
-        true_percent = 0.35
-        res = 0
-        if req_percent < true_percent:
-            res = 1         
-        return choice(regions), choice(job_titles), sal, la, per, req_percent, res
     
+    # отношение ежемесячного платежа к ЗП для получения положительного решения 
+    true_percent = 0.3
+    
+    # Генерим рандомные данные 
+    sal = choice(salary)
+    la = choice(loan_ammount)
+    per = choice(preiod)
+    
+    # Рассчитываем соотношение ежемесячного платежа к ЗП
+    req_percent = (la / per) / sal
+    
+    # готовим tuple для return
+    ret = choice(regions) +","+ choice(job_titles) +","+  str(sal) +","+  str(la) +","+ str(per)
+    
+    # если помимо данных для запроса КЗ нужно вернуть и решение 
+    if with_result == True:
+        
+        # Если соотношение запрошенных денег к ЗП меньше порога, то даем положительный результат 
+        if req_percent < true_percent:
+            return ret + ',1'
+        
+        # иначе отрицательный
+        else:         
+            return ret + ',0'
+    # иначе возвращаем просто данные для запроса КЗ
     else:
-        return choice(regions), choice(job_titles), choice(salary), choice(loan_ammount), choice(preiod)
+        return ret
 
 for x in range(10000):
     print(get_opty())
