1e6bc36bae5e5c131988070a4a22fe61714a92e7
Diman <dslads@gmail.com>
2020-03-30 19:37:03 +0300

    train ml model changes

--- python/train_ml_model.py
+++ python/train_ml_model.py
@@ -1,6 +1,51 @@
-# spark-submit  --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 D:\_git\andreev-ds-de-diploma\python\train_ml_model.py
+'''
+
+    Поскольку результат выдачи предварительнго одобрения в нашем случае линейно зависит от ЗП, Запрашиваемой суммы и периода кредита
+    мы не станем заморачиваться с подбором модели и возьмём старый добрый LogisticRegression из библиотеки SKLearn
+    
+    С подбором гипепараметров тоже заморачиваться не станем ибо:
+        1) у нас синтетические данные
+        2) у нас не стоит задачи сделать чёткую ML модель
+    
+    Для работы с данными воспользуемся всеми любимой библиотекой Pandas
+    
+'''
 
 import os
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+
+# формируем путь к файлу с данными
+data_path = os.path.join(os.getcwd(), '..', 'data', 'train.csv')
+
+# читаем данные  
+raw_data = pd.read_csv(data_path)
+
+# представим, что наши DS разведали и поняли, что на результат влияет только ЗП, Запрашиваемой суммы и периода кредита
+X = raw_data[['SALARY','LOAN_AMOUNT','PERIOD']]
+y = raw_data['TARGET']
+
+# разобьем наши данные на обучающие и тестовые
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# обучим нашу модель:
+clf = LogisticRegression(random_state=0).fit(X_train,y_train)
+
+# Оценим нашу модель
+print(cross_val_score(clf, X, y, cv=5, scoring='recall_macro'))
+
+#print(clf.predict(X_test))
+#print(X_train)
+#print(y_train)
+
+
+'''
+# spark-submit  --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 D:\_git\andreev-ds-de-diploma\python\train_ml_model.py
+
 from pyspark.sql import SparkSession
 
 # получаем спарковую сессию
@@ -24,12 +69,10 @@ schema = StructType([
     StructField("TARGET", IntegerType(), True)
 ])
     
-# формируем путь к файлу с данными
-data_path = os.path.join(os.getcwd(), '..', 'data', 'train.csv')
-
 # читаем данные 
 raw_data = spark.read.csv(data_path,header=True,schema=schema) \
     .selectExpr("UUID","REGION","JOB_TITLE","SALARY", "LOAN_AMOUNT", "PERIOD", "TARGET")
 
 raw_data.groupBy("REGION").count().show()
     
+'''
