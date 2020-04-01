71b48cd21b42d0228403993a42b175928d8e7037
Diman <dslads@gmail.com>
2020-04-01 14:08:57 +0300

    kafka_consumer_join.py created scripts for both topics

--- python/kafka_consumer_join.py
+++ python/kafka_consumer_join.py
@@ -1,6 +1,8 @@
 import os
 from pyspark.sql import SparkSession
+import pyspark.sql.functions
 from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, StringType
+from pyspark.sql.functions import udf
 
 # настройки для соединения с кафкой
 KAFKA_HOST = '34.71.139.131' 
@@ -14,6 +16,17 @@ data_root_path =  os.path.join(os.getcwd(), '..', 'data')
 parquet_path = os.path.join(data_root_path, '')
 checkpointLocation = os.path.join(data_root_path, 'checkpointOptyOutput')
 
+# Определяем функцию получения значений из CSV-стринги
+def get_from_csv(input_string, idx = 0, sep = ','):    
+    '''
+        Функция получает на вход строку input_string с данными, разделенными символом sep и возвращает значение idx "колонки"
+    '''
+    # читаем данные
+    return str(input_string.split(sep)[idx]) 
+ 
+# определяем UDF-функцию для использования в контексте спарка
+udf_get_from_csv = udf(lambda s, i: get_from_csv(s,i), StringType())
+
 # получаем спарковую сессию
 spark = SparkSession \
     .builder \
@@ -22,7 +35,14 @@ spark = SparkSession \
     
 print("Spark context started")
 
+# регистрируем нашу функцию
+spark.udf.register("udf_get_from_csv", udf_get_from_csv)
+print("\n\n\n - - - udf_get_from_csv registred succsessfully - - - \n\n\n")
+
+# -----------------------------
 # читаемм данные по КЗ из кафки
+# -----------------------------
+
 df_opty_in = spark \
   .readStream \
   .format("kafka") \
@@ -30,21 +50,47 @@ df_opty_in = spark \
   .option("subscribe", KAFKA_INPUT_TOPIC) \
   .load()
 
-df_opty_in.selectExpr("CAST(key AS STRING) as t1_key", "CAST(value AS STRING) as t1_value") \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start() \
-    .awaitTermination()
+# причёсываем наши данные по КЗ 
+df_opty_in.selectExpr( \
+        "CAST(key AS STRING) as t1_key" \
+        ,"CAST(udf_get_from_csv(CAST(value AS STRING), 0) AS STRING) as t1_region" \
+        ,"CAST(udf_get_from_csv(CAST(value AS STRING), 1) AS STRING) as t1_job_title" \
+        ,"CAST(udf_get_from_csv(CAST(value AS STRING), 2) AS STRING) as t1_salary" \
+        ,"CAST(udf_get_from_csv(CAST(value AS STRING), 3) AS STRING) as t1_loan_amount" \
+        ,"CAST(udf_get_from_csv(CAST(value AS STRING), 4) AS STRING) as t1_period") 
+  
     
-'''
+# ----------------------------------------    
 # читаемм данные с предсказаниями из кафки
+# ----------------------------------------
+    
 df_opty_out = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "{0}:{1}".format(KAFKA_HOST, KAFKA_PORT)) \
   .option("subscribe", KAFKA_OUTPUT_TOPIC) \
-  .load()
+  .load().selectExpr("CAST(key AS STRING) as t2_key", "CAST(value AS STRING) as t2_value") \
+    .writeStream \
+    .format("console") \
+    .start() \
+    .awaitTermination()
+  
+    
+'''
+# описываем структуру value в топике OptyInputTopic
+schema = StructType([
+    #StructField("UUID", StringType(), True),
+    StructField("REGION", StringType(), True),
+    StructField("JOB_TITLE", StringType(), True),
+    StructField("SALARY", FloatType(), True),
+    StructField("LOAN_AMOUNT", FloatType(), True),
+    StructField("PERIOD", IntegerType(), True)
+])
+
+    #.outputMode("complete") \
+
+
+
   
 df = df_opty_out.join(df_opty_in, $"mainKey" === $"joinedKey")
   
@@ -84,18 +130,7 @@ spark = SparkSession \
     
 print("Spark context started")
 
-# описываем структуру файла
-from pyspark.sql.types import StructType, StructField, IntegerType, LongType, StringType
 
-schema = StructType([
-    StructField("UUID", StringType(), True),
-    StructField("REGION", StringType(), True),
-    StructField("JOB_TITLE", StringType(), True),
-    StructField("SALARY", LongType(), True),
-    StructField("LOAN_AMOUNT", LongType(), True),
-    StructField("PERIOD", IntegerType(), True),
-    StructField("TARGET", IntegerType(), True)
-])
     
 # читаем данные 
 raw_data = spark.read.csv(data_path,header=True,schema=schema) \
